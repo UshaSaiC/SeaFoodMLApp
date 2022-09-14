d7efb15d9ac792691338839dd59e9f97227862f6
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-14 13:27:06 +0530

    Initial Commit

---
+++ SeaFoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFoodApp
+//
+//  Created by Usha Sai Chintha on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
