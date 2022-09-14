472674ebf90a6f30b5f39840a408cbdf7967b77b
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-14 14:13:11 +0530

    Added code wrt capturing image or uploading image from photo library

--- SeaFoodApp/ViewController.swift
+++ SeaFoodApp/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreML
+import Vision // it helps in processing images easily and allows us to use images to work with CoreML
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var cameraImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = .camera
+        } else {
+            imagePicker.sourceType = .photoLibrary
+        }
+        imagePicker.allowsEditing = false
     }
-
-
+    
+    // below function is triggered when user has selected an image and we can do something with that image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            self.cameraImageView.image = userPickedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
